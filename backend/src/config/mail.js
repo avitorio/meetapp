8c9b63b825c4c8cd44f8ab4e3682eddbb0264974
Andre Vitorio <andre@vitorio.net>
2019-10-17 18:19:09 +0200

    Added Mail variables to .env.example

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: '93d4685367be32',
-    pass: '6951a3c3f89d46',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   secure: false,
   default: {
